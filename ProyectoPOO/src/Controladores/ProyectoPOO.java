11fb709578810d72fe50bf0a010875d9ec1ffad3
Ricardo Espinoza <luriespi@espol.edu.ec>
2021-08-24 23:42:00 -0500

    creacion de clases basicas

---
+++ ProyectoPOO/src/Controladores/ProyectoPOO.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controladores;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ACER
+ */
+public class ProyectoPOO extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("INICIO");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+}
