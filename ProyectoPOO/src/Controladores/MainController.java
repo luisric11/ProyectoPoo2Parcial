cda5eb3285512debfa13ef5fba313badbb434650
Justo Moran Zavala <justomoran2016@gmail.com>
2021-08-25 14:39:25 -0500

    Llenado de ComboBox y adicion de elementos a clase Controller

--- ProyectoPOO/src/Controladores/MainController.java
+++ ProyectoPOO/src/Controladores/MainController.java
@@ -7,7 +7,14 @@ package Controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -16,12 +23,40 @@ import javafx.fxml.Initializable;
  */
 public class MainController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TextField ciudad;
+    
+    @FXML
+    private TextField alcalde;
+    
+    @FXML
+    private ComboBox dificultad;
+    
+    @FXML
+    private Text presupuestoText;
+    
+    @FXML
+    private Text gastosText;
+    
+    @FXML
+    private Button boton1;
+    
+    @FXML
+    private Button cambiarServicio;
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        llenarDificultad();
+        
     }    
+    
+    private void llenarDificultad(){
+        ObservableList<String> cons = FXCollections.observableArrayList();
+        cons.add(Clases.Dificultad.FACIL.toString());
+        cons.add(Clases.Dificultad.INTERMEDIO.toString());
+        cons.add(Clases.Dificultad.DIFICIL.toString());
+        dificultad.getItems().addAll(cons);
+    }
     
 }
