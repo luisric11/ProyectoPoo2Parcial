495932c6b8c603beb02917edeac79fe10996a841
Justo Moran Zavala <justomoran2016@gmail.com>
2021-08-28 15:22:25 -0500

    Llenado de cesped ciudad

--- ProyectoPOO/src/Controladores/MainController.java
+++ ProyectoPOO/src/Controladores/MainController.java
@@ -14,7 +14,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import Clases.Administrador;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
 /**
  * FXML Controller class
@@ -44,11 +54,23 @@ public class MainController implements Initializable {
     @FXML
     private Button cambiarServicio;
     
+    @FXML
+    private BorderPane pane;
+    
+    GridPane gridpane;
+    
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        llenarDificultad();
-        
+        try {
+            gridpane = new GridPane();
+            pane.setCenter(gridpane);
+            llenarDificultad();     
+            inicializargridPane();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
     
     private void llenarDificultad(){
@@ -57,6 +79,26 @@ public class MainController implements Initializable {
         cons.add(Clases.Dificultad.INTERMEDIO.toString());
         cons.add(Clases.Dificultad.DIFICIL.toString());
         dificultad.getItems().addAll(cons);
+    }
+    
+    private void inicializargridPane() throws FileNotFoundException{
+        gridpane.getColumnConstraints().clear();
+        for(int i=0;i<Administrador.COLUMNAS;i++){
+            for(int j=0;j<Administrador.FILAS;j++){
+                try {
+                    StackPane stackPane= new StackPane();
+                    Image image = new Image(new FileInputStream("src/graphicResources/grassTile.png"));
+                    ImageView imagen = new ImageView(image);
+                    imagen.setFitHeight(40);
+                    imagen.setFitWidth(40);
+                    stackPane.getChildren().add(imagen);
+                    gridpane.add(stackPane,i, j);
+                } catch (FileNotFoundException ex) {
+                    System.exit(0);
+                    Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
     }
     
 }
